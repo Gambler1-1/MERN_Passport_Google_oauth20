8a42d96bd0613ebac26364ccebfc1c24d99b4727
Mohammad Abdullah <m.abdullah0747@gmail.comm>
2022-11-14 17:59:16 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,8 @@
+Fetched User : {
+"id":"117356677700713159691",
+"displayName":"AGILE FALCON YT",
+"name":{"givenName":"AGILE FALCON YT"},
+"emails":[{"value":"m.abdullah0747@gmail.com","verified":true}],
+"photos":[{"value":"https://lh3.googleusercontent.com/a/ALm5wu3oMCjj3NjeEP6zhsSUPUa-Jfb0kbcO97fZiIpCoQ=s96-c"}],
+"provider":"google",
+"_raw":"{\n  \"sub\": \"117356677700713159691\",\n  \"name\": \"AGILE FALCON YT\",\n  \"given_name\": \"AGILE FALCON YT\",\n  \"picture\": \"https://lh3.googleusercontent.com/a/ALm5wu3oMCjj3NjeEP6zhsSUPUa-Jfb0kbcO97fZiIpCoQ\\u003ds96-c\",\n  \"email\": \"m.abdullah0747@gmail.com\",\n  \"email_verified\": true,\n  \"locale\": \"en\"\n}","_json":{"sub":"117356677700713159691","name":"AGILE FALCON YT","given_name":"AGILE FALCON YT","picture":"https://lh3.googleusercontent.com/a/ALm5wu3oMCjj3NjeEP6zhsSUPUa-Jfb0kbcO97fZiIpCoQ=s96-c","email":"m.abdullah0747@gmail.com","email_verified":true,"locale":"en"}}
